1116d3cf7b63cfdd4d5f0d1ebf82d6c8b5ac666e
studentdot0403 <studentdot0403@de.techni.dom>
2018-11-08 16:25:59 +0100

    Add project files.

---
+++ LPT_Game_BLL/WorldGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPT_Game_BLL
+{
+    class WorldGenerator
+    {
+        World world = new World();
+    }
+}

330b281d96aafefd86b05ffc6708e7de95cf0414
Rakart0 <rakart0@gmail.com>
2018-11-15 18:54:41 +0100

    Forgot to push physics files

---
+++ LTP_BLL/PhysicsUpdater.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTP_BLL
+{
+    public class PhysicsUpdater
+    {
+        public List<GameObjectComponent> PhysicsObjects;
+        public PhysicsUpdater()
+        {
+            PhysicsObjects = new List<GameObjectComponent>();
+        }
+
+        public void UpdatePhysics()
+        {
+            foreach (GameObjectComponent g in PhysicsObjects)
+            {
+                g.Update();
+            }
+        }
+
+    }
+}
